ceab2bd87c1073f52a6e34a621ccfb113a8732f5
Simon Coulton <simon.coulton@gmail.com>
2014-01-21 23:05:36 +1100

    find_packages instead of hardcoded

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
-from setuptools import setup, Command
+from setuptools import setup, Command, find_packages
 import watson.common
 
 
@@ -100,7 +100,7 @@ setup(
               'dict',
               'context managers'],
 
-    packages=['watson', 'watson.common'],
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     include_package_data=True,
 
     zip_safe=False,
