6e7667420b881e59b2c8ecdb35b4e0077d59f7f2
Simon Coulton <simon.coulton@gmail.com>
2014-02-21 16:59:11 +1100

    JSON serialize/deserialize capabilities for objects

--- tests/watson/common/support.py
+++ tests/watson/common/support.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 # Support functions, classes
+from datetime import datetime
+from watson.common import json
 
 
 def some_func():
@@ -7,3 +9,39 @@ def some_func():
 
 
 DATA = {}
+
+
+def date_serialize(value, format='%d/%m/%Y', timezone=None):
+    return value.strftime(format)
+
+
+def date_deserialize(value, format='%d/%m/%Y'):
+    return datetime.strptime(value, format)
+
+
+def data_list_serialize(value, **kwargs):
+    return [
+        json.serialize(v,
+                       attributes=('name', 'date'),
+                       strategies={'date': date_serialize}, **kwargs)
+        for v in value]
+
+
+def data_dict_serialize(value, **kwargs):
+    return {
+        k: json.serialize(v,
+                          attributes=('name', 'date',),
+                          strategies={'date': date_serialize}, **kwargs)
+        for k, v in value.items()}
+
+
+class MyObject(object):
+    name = None
+    date = None
+    data_list = None
+    data_dict = None
+    private = None
+
+    def __init__(self, name=None, date=None):
+        self.name = name
+        self.date = date
---
+++ tests/watson/common/test_json.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from tests.watson.common import support
+from watson.common import json
+
+
+class TestSerialize(object):
+    def test_basic(self):
+        obj = support.MyObject('test')
+        d = json.serialize(obj, ('name',))
+        assert d == {'name': 'test'}
+
+    def test_complex(self):
+        obj = support.MyObject('test', date=datetime.now())
+        d = json.serialize(obj, ('name', 'date'),
+                           strategies={'date': support.date_serialize})
+        assert isinstance(d['date'], str)
+
+    def test_list(self):
+        obj = support.MyObject('test', date=datetime.now())
+        obj.data_list = [
+            support.MyObject('testing', date=datetime.now()),
+            support.MyObject('again', date=datetime.now())
+        ]
+        d = json.serialize(obj, ('name', 'date', 'data_list'),
+                           strategies={
+                                       'data_list': support.data_list_serialize,
+                                       'date': support.date_serialize})
+        assert isinstance(d['dataList'][0]['date'], str)
+
+    def test_dict(self):
+        obj = support.MyObject('test', date=datetime.now())
+        obj.data_dict = {
+            'test': support.MyObject('testing', date=datetime.now()),
+        }
+        d = json.serialize(obj,
+                           ('name', 'date', 'data_dict'),
+                           strategies={'data_dict': support.data_dict_serialize,
+                                       'date': support.date_serialize})
+        assert isinstance(d['dataDict']['test']['date'], str)
+
+
+class TestDeserialize(object):
+    def test_basic(self):
+        d = {'name': 'test'}
+        obj = json.deserialize(d, class_=support.MyObject,
+                               attributes=('name',))
+        assert isinstance(obj, support.MyObject)
+        assert obj.name == 'test'
+        assert not obj.date
+
+    def test_complex(self):
+        d = {'name': 'test', 'date': '1/1/2014'}
+        obj = json.deserialize(d, class_=support.MyObject,
+                               attributes=('name', 'date'),
+                               strategies={'date': support.date_deserialize})
+        assert isinstance(obj.date, datetime)
